a98d3be3d913ef1078e9dc6609ca1ddbb27bdf06
tecresearch <bnlv1212@gmail.com>
2025-02-28 03:35:31 +0530

    maven project created successfully

---
+++ hibernate/HP-a0-hibernate-demo/src/main/java/in/tecresearch/App.java
@@ -0,0 +1,13 @@
+package in.tecresearch;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
